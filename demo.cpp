02e97ff37b21c13a5e4ac82a58a7f27727ac8482
Akshay <akshaysawant003@gmail.com>
2018-10-13 20:20:43 +0530

    Demo cpp modified

--- demo.cpp
+++ demo.cpp
@@ -4,7 +4,14 @@
 // macro definition
 
 int main()
+/*return_type function_name(){
+    //Statements or codes
+}*/
 {
 	std::cout<<"C++ is better than C";
+	/*count means c++ output*/
+	/*std means standard code/output */
+	/*:: means scope operator*/
+
 	return 0;
 }
